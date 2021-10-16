b91972369f63684c8381968ccbcef88a35a2a8ca
Touhedur Rahman <mtrshuvo@ymail.com>
2021-10-16 15:24:17 +0600

    changes

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -25,14 +25,14 @@ module.exports.ipn = async(req, res)=>{
 }
 
 module.exports.successPayment = async(req, res)=>{
-    res.sendFile(path.join(__basedir+"public/success.html"));
+    res.sendFile(path.join(__basedir+"/public/success.html"));
 }
 module.exports.failedPayment = async(req, res)=>{
-    res.sendFile(path.join(__basedir+"public/falied.html"));
+    res.sendFile(path.join(__basedir+"/public/falied.html"));
 
 }
 module.exports.cancelPayment = async(req, res)=>{
-    res.sendFile(path.join(__basedir+"public/cancel.html"));
+    res.sendFile(path.join(__basedir+"/public/cancel.html"));
 
 }
 module.exports.initPayment = async (req, res) => {
